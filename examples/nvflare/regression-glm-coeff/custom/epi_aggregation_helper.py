c03cbf2fc0b2258afc67e28d1f7b1194343c963d
Noy-Maimon <72340690+Noy-Maimon@users.noreply.github.com>
2023-10-05 10:23:37 +0300

    Update examples/nvflare/regression-glm-coeff/custom/epi_aggregation_helper.py
    Co-authored-by: Yuval Baror <78553214+yuvalrhino@users.noreply.github.com>

--- examples/nvflare/regression-glm-coeff/custom/epi_aggregation_helper.py
+++ examples/nvflare/regression-glm-coeff/custom/epi_aggregation_helper.py
@@ -1,3 +1,6 @@
+# Copyright (c) 2023, Rhino HealthTech, Inc.
+# Original file modified by Rhino Health to adapt it to the Rhino Health Federated Computing Platform.
+
 # Copyright (c) 2022, NVIDIA CORPORATION.  All rights reserved.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
