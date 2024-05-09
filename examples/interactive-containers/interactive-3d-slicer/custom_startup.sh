6c9f7a80a5b0ff84baaca9040437666be7083db6
Noy-Maimon <noy@rhinohealth.com>
2024-05-09 16:40:19 +0300

    exclude cohort_data.csv and dataset.csv

--- examples/interactive-containers/interactive-3d-slicer/custom_startup.sh
+++ examples/interactive-containers/interactive-3d-slicer/custom_startup.sh
@@ -2,7 +2,10 @@
 # 3D Slicer does not properly overwrite output files when they are soft links and not regular files.
 # Since by default FCP sets up soft links from /output to /input, this causes a problem for 3D Slicer. To overcome this, we will delete the soft links in the /output directory and instead copy the relevant files from /input to /output as regular files.
 rm -r /output/*
-# Now we can copy the directories under /input to /output
-find /input/ -mindepth 1 -type d -exec cp {} /output/ \;
+# Copy the input directory content to the output directory.
+cp -r /input/* /output/
+# Exclude the dataset files (they exists in the /input directory but we do not automatically store them in the /output directory).
+rm -r /output/dataset.csv
+rm -r /output/cohort_data.csv.csv
 
 exec /startup.sh
