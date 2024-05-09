c065d28b60cf2dba841f8b6e6642e40059b629a5
Noy-Maimon <72340690+Noy-Maimon@users.noreply.github.com>
2024-05-09 11:34:51 +0300

    Update examples/interactive-containers/interactive-3d-slicer/custom_startup.sh
    Co-authored-by: Yuval Baror <78553214+yuvalrhino@users.noreply.github.com>

--- examples/interactive-containers/interactive-3d-slicer/custom_startup.sh
+++ examples/interactive-containers/interactive-3d-slicer/custom_startup.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
-# The content of /output is soft links to files in /input, hence copy will be skipped - so we delete the output dir first
+# 3D Slicer does not properly overwrite output files when they are soft links and not regular files.
+# Since by default FCP sets up soft links from /output to /input, this causes a problem for 3D Slicer. To overcome this, we will delete the soft links in the /output directory and instead copy the relevant files from /input to /output as regular files.
 rm -r /output/*
 # Now we can copy the files
 cp -r /input/* /output/
