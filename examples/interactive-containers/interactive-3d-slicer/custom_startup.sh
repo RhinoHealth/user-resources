b769eb1c13b43703d1fbdd1aa8a1f97c0b4d7b28
Noy-Maimon <noy@rhinohealth.com>
2024-05-09 10:32:01 +0300

    custom script

--- examples/interactive-containers/interactive-3d-slicer/custom_startup.sh
+++ examples/interactive-containers/interactive-3d-slicer/custom_startup.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
-cp -r /input/. /output/
+# The content of /output is soft links to files in /input, hence copy will be skipped - so we delete the output dir first
+rm -r /output/*
+# Now we can copy the files
+cp -r /input/* /output/
 exec /startup.sh
