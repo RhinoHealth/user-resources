91bab28cc2998dcf54cfcef7c9c6db528865490b
Noy-Maimon <noy@rhinohealth.com>
2024-05-16 18:23:09 +0200

    manually create links

--- examples/interactive-containers/interactive-3d-slicer/custom_startup.sh
+++ examples/interactive-containers/interactive-3d-slicer/custom_startup.sh
@@ -3,9 +3,25 @@
 # Since by default FCP sets up soft links from /output to /input, this causes a problem for 3D Slicer. To overcome this, we will delete the soft links in the /output directory and instead copy the relevant files from /input to /output as regular files.
 rm -r /output/*
 # Copy the input directory content to the output directory.
-cp -r /input/* /output/
-# Exclude the dataset files (they exists in the /input directory but we do not automatically store them in the /output directory).
-rm -r /output/dataset.csv
-rm -r /output/cohort_data.csv.csv
+
+mkdir -p /output/0/dicom_data
+mkdir -p /output/0/files_data
+
+cp -r /input/0/dicom_data/* /output/0/dicom_data/
+cp -r /input/0/files_data/* /output/0/files_data/
+
+# Create the directory structure in the destination directory
+cd "/output/0/dicom_data" || exit
+find . -type d -exec mkdir -p "/output/dicom_data/{}" \;
+
+# Create symbolic links for all files
+find . -type f -exec ln -s "/output/0/dicom_data/{}" "/output/dicom_data/{}" \;
+
+# Create the directory structure in the destination directory
+cd "/output/0/files_data" || exit
+find . -type d -exec mkdir -p "/output/files_data/{}" \;
+
+# Create symbolic links for all files
+find . -type f -exec ln -s "/output/0/files_data/{}" "/output/files_data/{}" \;
 
 exec /startup.sh
