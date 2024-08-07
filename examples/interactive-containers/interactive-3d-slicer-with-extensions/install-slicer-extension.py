6229f3ef94aa0f9ef0f3056f9667a15d3d743fb5
Tal Einat <tal@rhinohealth.com>
2024-08-07 11:09:51 +0300

    Update 3DSlicer interactive container examples to use improved entrypoint script

--- examples/interactive-containers/interactive-3d-slicer-with-extensions/install-slicer-extension.py
+++ examples/interactive-containers/interactive-3d-slicer-with-extensions/install-slicer-extension.py
@@ -1,8 +1,8 @@
 extension_names = [
-  ("DCMQI", "/root/Desktop/31382-linux-amd64-DCMQI-git451bf84-2023-01-26.tar.gz"),
-  ("PETDICOMExtension", "/root/Desktop/31382-linux-amd64-PETDICOMExtension-git6840644-2022-03-18.tar.gz"),
-  ("QuantitativeReporting", "/root/Desktop/31382-linux-amd64-QuantitativeReporting-git8a6e612-2022-11-30.tar.gz"),
-  ("SlicerDevelopmentToolbox", "/root/Desktop/31382-linux-amd64-SlicerDevelopmentToolbox-git2c2465d-2022-09-14.tar.gz")
+  ("DCMQI", "/opt/slicer-extensions/31382-linux-amd64-DCMQI-git451bf84-2023-01-26.tar.gz"),
+  ("PETDICOMExtension", "/opt/slicer-extensions/31382-linux-amd64-PETDICOMExtension-git6840644-2022-03-18.tar.gz"),
+  ("QuantitativeReporting", "/opt/slicer-extensions/31382-linux-amd64-QuantitativeReporting-git8a6e612-2022-11-30.tar.gz"),
+  ("SlicerDevelopmentToolbox", "/opt/slicer-extensions/31382-linux-amd64-SlicerDevelopmentToolbox-git2c2465d-2022-09-14.tar.gz")
 ]
 
 manager = slicer.app.extensionsManagerModel()
