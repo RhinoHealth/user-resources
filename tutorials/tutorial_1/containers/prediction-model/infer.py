4ccc2dd20acad7dc48410d976cd4697df2aeb0dd
Dan Runt <dan@rhinohealth.com>
2024-12-24 10:34:26 -0500

    Updates tutorial 1 to NVFlare 2.5

--- tutorials/tutorial_1/containers/prediction-model/infer.py
+++ tutorials/tutorial_1/containers/prediction-model/infer.py
@@ -1,35 +1,43 @@
 #!/usr/bin/env python
-import shutil
 import sys
-from pathlib import Path
 
 import pandas as pd
 import torch
 import torchvision
-from torch.utils.data.dataloader import DataLoader
-from torchvision.transforms import ToTensor, Normalize, Compose,  Resize, RandomRotation, CenterCrop
-
 from network import PneumoniaModel
+from torch.utils.data.dataloader import DataLoader
+from torchvision.transforms import (
+    CenterCrop,
+    Compose,
+    Normalize,
+    RandomRotation,
+    Resize,
+    ToTensor,
+)
 
 
-def infer(model_weights_file_path):
+def infer(model_params_file_path):
     # Setup the model
     model = PneumoniaModel()
-    model.load_state_dict(torch.load(model_weights_file_path)["model"])
+    model.load_state_dict(torch.load(model_params_file_path)["model"])
     model.eval()
     device = torch.device("cuda") if torch.cuda.is_available() else torch.device("cpu")
     model.to(device)
 
     # Preparing the dataset for testing.
-    transforms = Compose([
-        Resize(size=(256, 256)),
-        RandomRotation(degrees=(-20, +20)),
-        CenterCrop(size=224),
-        ToTensor(),
-        Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
-    ])
+    transforms = Compose(
+        [
+            Resize(size=(256, 256)),
+            RandomRotation(degrees=(-20, +20)),
+            CenterCrop(size=224),
+            ToTensor(),
+            Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225]),
+        ]
+    )
     tabular_data = pd.read_csv("/input/dataset.csv")
-    dataset = torchvision.datasets.ImageFolder(root="/input/file_data", transform=transforms)
+    dataset = torchvision.datasets.ImageFolder(
+        root="/input/file_data", transform=transforms
+    )
     loader = DataLoader(dataset, batch_size=4, shuffle=False)
 
     # Inference: Apply model and add scores column.
@@ -37,17 +45,16 @@ def infer(model_weights_file_path):
     with torch.no_grad():
         for i, (images, labels) in enumerate(loader):
             images = images.to(device)
-            output = model(images)
-            batch_scores = torch.select(output, 1, 1)
+            predictions = model(images)
+            batch_scores = torch.select(predictions, 1, 1)
             scores.extend([score.item() for score in batch_scores])
-    tabular_data['Model Score'] = scores
+    tabular_data["Model Score"] = scores
 
     tabular_data.to_csv("/output/dataset.csv", index=False)
 
 
-
 if __name__ == "__main__":
     args = sys.argv[1:]
-    (model_weights_file_path,) = args
-    infer(model_weights_file_path)
+    (model_params_file_path,) = args
+    infer(model_params_file_path)
     sys.exit(0)
