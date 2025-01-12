37a69594389a999703730c07fa30f2e6d9779a63
Noy-Maimon <noy@rhinohealth.com>
2025-01-12 18:26:20 +0200

    spaces

--- examples/nvflare/regression-glm-coeff/custom/regression_trainer.py
+++ examples/nvflare/regression-glm-coeff/custom/regression_trainer.py
@@ -117,8 +117,6 @@ class GLMTrainer(Executor):
         - Ensure that the supplied variables (either from formula or x_values and y_values) are present in the dataset.
         - Ensure that the supplied variables do not contain invalid signs that will cause the model to fail.
         - Ensure that the offset is only used with the Poisson distribution family.
-
-
         """
         # Validate either formula or explicit columns are supplied
         if not self.formula and not (self.x_values and self.y_values):
@@ -126,8 +124,8 @@ class GLMTrainer(Executor):
             raise ValueError("Either formula or x_values and y_values must be provided.")
 
         # Validate formula structure
-        if formula:
-            formula = self.formula.rstrip(" ") # TODO: better function for cleaning spaces
+        if self.formula:
+            formula = self.formula.replace(" ", "")
             dependent_var, independent_vars = formula.split("~")
             formula_parts = independent_vars.split("+") + [dependent_var]
         else:
