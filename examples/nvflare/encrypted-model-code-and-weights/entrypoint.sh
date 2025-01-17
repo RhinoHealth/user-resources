7368505e4c8f134009825fbda80c41478d01d844
Dan Runt <dan@rhinohealth.com>
2025-01-17 15:30:27 -0500

    Adds NVFlare 2.5 files

--- examples/nvflare/encrypted-model-code-and-weights/entrypoint.sh
+++ examples/nvflare/encrypted-model-code-and-weights/entrypoint.sh
@@ -2,6 +2,6 @@
 set -eux -o pipefail
 
 # Decrypt all files with a .enc extension.
-find . -type f -name '*.enc' -exec bash -c 'python ./custom/decrypt_code.py "$1" "${1%.enc}"' bash {} ';'
+find . -type f -name '*.enc' -exec bash -c 'python ./app/custom/decrypt_code.py "$1" "${1%.enc}"' bash {} ';'
 
 exec "$@"
--- examples/nvflare/encrypted-model-code-and-weights/infer.py
+++ examples/nvflare/encrypted-model-code-and-weights/infer.py
@@ -1,49 +1,43 @@
 #!/usr/bin/env python
 import sys
-from pathlib import Path
-from cryptography.fernet import Fernet
-import json
+
 import pandas as pd
 import torch
 import torchvision
-from torch.utils.data.dataloader import DataLoader
-from torchvision.transforms import ToTensor, Normalize, Compose,  Resize, RandomRotation, CenterCrop
-
 from network import PneumoniaModel
-
-
-def decrypt_weights(model_parameters_path):
-    secret_run_params_file_path = Path("/input/secret_run_params.json")
-    if secret_run_params_file_path.is_file():
-        with secret_run_params_file_path.open("rb") as secret_run_params_file:
-            secret_run_params = json.load(secret_run_params_file)
-            key = secret_run_params["key"]
-            fernet = Fernet(key)
-            encrypted = Path(model_parameters_path).read_bytes()
-            decrypted = fernet.decrypt(encrypted)
-            model_parameters_path = '/output/model_parameters.pt'
-            Path(model_parameters_path).write_bytes(decrypted)
-    return model_parameters_path
+from torch.utils.data.dataloader import DataLoader
+from torchvision.transforms import (
+    CenterCrop,
+    Compose,
+    Normalize,
+    RandomRotation,
+    Resize,
+    ToTensor,
+)
 
 
 def infer(model_params_file_path):
     # Setup the model
     model = PneumoniaModel()
-    model.load_state_dict(torch.load(decrypt_weights(model_params_file_path))["model"])
+    model.load_state_dict(torch.load(model_params_file_path)["model"])
     model.eval()
     device = torch.device("cuda") if torch.cuda.is_available() else torch.device("cpu")
     model.to(device)
 
     # Preparing the dataset for testing.
-    transforms = Compose([
-        Resize(size=(256, 256)),
-        RandomRotation(degrees=(-20, +20)),
-        CenterCrop(size=224),
-        ToTensor(),
-        Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
-    ])
+    transforms = Compose(
+        [
+            Resize(size=(256, 256)),
+            RandomRotation(degrees=(-20, +20)),
+            CenterCrop(size=224),
+            ToTensor(),
+            Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225]),
+        ]
+    )
     tabular_data = pd.read_csv("/input/dataset.csv")
-    dataset = torchvision.datasets.ImageFolder(root="/input/file_data", transform=transforms)
+    dataset = torchvision.datasets.ImageFolder(
+        root="/input/file_data", transform=transforms
+    )
     loader = DataLoader(dataset, batch_size=4, shuffle=False)
 
     # Inference: Apply model and add scores column.
@@ -51,17 +45,16 @@ def infer(model_params_file_path):
     with torch.no_grad():
         for i, (images, labels) in enumerate(loader):
             images = images.to(device)
-            output = model(images)
-            batch_scores = torch.select(output, 1, 1)
+            predictions = model(images)
+            batch_scores = torch.select(predictions, 1, 1)
             scores.extend([score.item() for score in batch_scores])
-    tabular_data['Model_Score'] = scores
+    tabular_data["Model Score"] = scores
 
     tabular_data.to_csv("/output/dataset.csv", index=False)
 
 
-
 if __name__ == "__main__":
     args = sys.argv[1:]
-    (model_weights_file_path,) = args
-    infer(model_weights_file_path)
+    (model_params_file_path,) = args
+    infer(model_params_file_path)
     sys.exit(0)
