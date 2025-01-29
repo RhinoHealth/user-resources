12ef519afc4b865c2e62c5b65addf82a584bd899
oriashush <84438534+oriashush@users.noreply.github.com>
2025-01-30 00:39:31 +0200

    Update strategy.py
    Added licesnse

--- examples/nvflare/hello-flower/app/custom/flwr_pt/strategy.py
+++ examples/nvflare/hello-flower/app/custom/flwr_pt/strategy.py
@@ -1,3 +1,20 @@
+# Original file modified by Rhino to adapt it to the Rhino Federated Computing Platform.
+# Original file: https://github.com/adap/flower/blob/main/framework/docs/source/how-to-save-and-load-model-checkpoints.rst shared under the Apache 2.0 license
+
+# Copyright 2024 Flower Labs GmbH. All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
 import flwr as fl
 from flwr.server.client_proxy import ClientProxy
 from flwr.common import FitRes, Parameters, Scalar
