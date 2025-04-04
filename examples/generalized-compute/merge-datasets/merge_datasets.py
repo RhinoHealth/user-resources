39b956da47d079fe3756e9ac490d126075d785b1
Daniel Feller <daniel@Daniels-Laptop.local>
2025-04-04 14:25:24 -0400

    Removed archived generalized-compute containers

--- examples/generalized-compute/merge-datasets/merge_datasets.py
+++
@@ -1,7 +0,0 @@
-import pandas as pd
-
-if __name__ == "__main__":
-    first = pd.read_csv("/input/0/dataset.csv")
-    second = pd.read_csv("/input/1/dataset.csv")
-    merged = pd.concat([first, second], ignore_index=True, axis=0)
-    merged.to_csv("/output/0/dataset.csv", index=False)
