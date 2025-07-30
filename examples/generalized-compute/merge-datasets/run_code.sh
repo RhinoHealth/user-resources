740b3b6217e0121c29f4017d65b956dfcd285251
ddavid37 <dd3269@columbia.edu>
2025-07-30 12:32:47 -0400

    Remove unrelated run_code.sh from branch

--- examples/generalized-compute/merge-datasets/run_code.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-DIR=$(dirname "$(readlink -f "$BASH_SOURCE")")
-
-set -x  # print each command (debugging)
-set -e  # stop on error
-
-python $DIR/merge_datasets.py --input_dir /input --output_dir /output
