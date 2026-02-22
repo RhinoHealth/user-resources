5640e9f2b23e969000bb87e17e30eba017a0c5e7
Yaniv Greenberg <yaniv@rhinohealth.com>
2026-02-22 12:21:47 +0200

    Update upload-file script following bucket structure change

--- rhino-utils/upload-file-to-s3.sh
+++ rhino-utils/upload-file-to-s3.sh
@@ -3,12 +3,13 @@ set -eu -o pipefail
 
 # Function to display usage
 usage() {
-    echo "Usage: $0 <upload_directory> <s3_bucket> <path_in_bucket>"
+    echo "Usage: $0 <upload_directory> <storage_bucket> <bucket_prefix> <path_in_bucket>"
     echo
     echo "Parameters:"
     echo "<upload_directory>   The local directory to upload."
-    echo "<s3_bucket>         The name of the S3 bucket."
-    echo "<path_in_bucket>    The path in the S3 bucket where the directory should be uploaded."
+    echo "<storage_bucket>     The name of the storage bucket."
+    echo "<bucket_prefix>      The provided workgroup prefix inside the storage bucket."
+    echo "<path_in_bucket>     Your desired path in the S3 bucket where the directory should be uploaded."
     exit 1
 }
 
@@ -19,8 +20,9 @@ fi
 
 # Assign the arguments to variables
 upload_directory=$1
-s3_bucket=$2
-path_in_bucket=$3
+storage_bucket=$2
+bucket_prefix=$3
+path_in_bucket=$4
 
 if [ -z ${AWS_ACCESS_KEY_ID+x} ];  then
     echo "The 'AWS_ACCESS_KEY_ID' environment variable is empty - please set it to a valid value"
@@ -34,7 +36,7 @@ fi
 
 set -x
 # Use the AWS CLI to upload the directory to the S3 bucket
-if aws s3 cp --recursive "$upload_directory" "s3://$s3_bucket/$path_in_bucket/"; then
+if aws s3 cp --recursive "$upload_directory" "s3://$storage_bucket/$bucket_prefix/$path_in_bucket/"; then
     echo "Upload completed."
 else
     echo "Upload failed. Please check your parameters and try again."
