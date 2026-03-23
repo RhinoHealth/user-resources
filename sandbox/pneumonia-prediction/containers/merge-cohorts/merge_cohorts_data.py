78b9d1674736a2bb3e3a5ba74ede542a9b9df550
Dan Runt <dan@rhinohealth.com>
2026-03-23 16:03:08 -0400

    Restructure validation directory

--- sandbox/pneumonia-prediction/containers/merge-cohorts/merge_cohorts_data.py
+++
@@ -1,8 +0,0 @@
-import pandas as pd
-
-if __name__ == "__main__":
-    first = pd.read_csv("/input/0/cohort_data.csv")
-    second = pd.read_csv("/input/1/cohort_data.csv")
-    third = pd.read_csv("/input/2/cohort_data.csv")
-    merged = pd.concat([first, second, third], ignore_index=True, axis=0)
-    merged.to_csv("/output/0/cohort_data.csv", index=False)
